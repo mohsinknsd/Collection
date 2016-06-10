0ab36a5a2cb7169e7746191e724c6f4e9c2e13e8
Mohsin Khan <khan.square@gmail.com>
2016-06-10 17:12:30 +0530

    Log file support

--- Log.java
+++ Log.java
@@ -1,10 +1,18 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.text.MessageFormat;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 import java.util.Properties;
+
+import org.apache.commons.lang3.SystemUtils;
 
 import kafka.producer.KeyedMessage;
 
@@ -17,16 +25,27 @@ import kafka.producer.KeyedMessage;
  * <li><b>Log.w</b> for warnings</li>
  * <li><b>Log.d</b> for debug</li>
  * </ul>
+ * 
+ * Note : debug logs will not be written into the files
+ * 
  * @author mohsin.khan
  * @date 2 June 2016
  */
 
 public class Log {
+	
 	/**
-	 * A collection of all messages that will be logged to the console.
+	 * If log writing in files is enabled in main configuration file, then this {@code path} will be initialized and 
+	 * logger will write files of logs on this file path 
 	 */
-	private static Properties messages = new Properties(); 
-
+	private static String path = null;
+	
+	/**
+	 * It is max class name length limit in the logs. If class name is less than this limit, then remaining length
+	 * will be covered by dots(.) using {@code get()} method. 
+	 */
+	private static final int LIMIT = 12;
+	
 	/**
 	 * Date format in the whole application can be configured from the config.properties file.
 	 * Here formatter will be initialized using default date format.
@@ -34,37 +53,50 @@ public class Log {
 	private static SimpleDateFormat formatter;
 	
 	/**
-	 * It is max class name length limit in the logs. If class name is less than this limit, then remaining length
-	 * will be covered by dots(.) using {@code get()} method. 
+	 * A collection of all messages that will be logged to the console.
 	 */
-	private static final int LIMIT = 12;
-
+	private static Properties messages = new Properties();
+	
+	private static final String INF = "INF";
+	private static final String ERR = "ERR";
+	private static final String WRN = "WRN";
+	private static final String DBG = "DBG";
+	
 	/**
 	 * Loading all messages at the beginning for the faster access to messages.properties file.
 	 * This instance will be used to fetch messages using their keys.
 	 */
 	static {
 		try {
-			messages.load(Class.class.getResourceAsStream("/config.properties"));			
+			//first loading configuration to get default configuration of logger and date time format 
+			messages.load(new FileReader(new File("resources/config.properties")));
+			path = Boolean.parseBoolean(messages.getProperty("write.log.files")) ? messages.getProperty("log.file.dir") 
+					+ ((SystemUtils.IS_OS_WINDOWS) ? "iot-hub//" : "iot-hub\\") : null;			
 			formatter = new SimpleDateFormat(messages.getProperty("display.date.format"), Locale.US);
-			messages.load(Class.class.getResourceAsStream("/messages.properties"));
-		} catch (IOException e) {			
+			
+			//now loading all messages
+			messages.load(new FileReader(new File("resources/messages.properties")));
+		} catch (IOException | ExceptionInInitializerError e) {			
 			e.printStackTrace();
 		}
 	}
 
-	private static void out(Class<?> c, String s) {
-		/*System.out.println("[" + formatter.format(new Date(System.currentTimeMillis()))
-		+ "]-[INF]-[" + c.getSimpleName() + "] " + s);*/
-		System.out.printf("[" + formatter.format(new Date(System.currentTimeMillis()))
-		+ "]-[INF]-[%-"+LIMIT+"."+LIMIT+"s] %s%n", get(c.getSimpleName()), s);
+	private synchronized static String out(Class<?> c, String x, String s) {
+		/*System.out.printf("[" + formatter.format(new Date(System.currentTimeMillis()))
+		+ "]-[INF]-[%-"+LIMIT+"."+LIMIT+"s] %s%n", get(c.getSimpleName()), s);*/
+		String log = "[" + formatter.format(new Date(System.currentTimeMillis()))
+		+ "]-[" + x + "]-[" + get(c.getSimpleName()).substring(0, LIMIT)  + "] " + s + "\n";
+		System.out.print(log);
+		return log;
 	}
 
-	private static void err(Class<?> c, String x, String s) {
-		/*System.err.println("[" + formatter.format(new Date(System.currentTimeMillis()))
-		+ "]-[WRN]-[" + c.getSimpleName() + "] " + s);*/
-		System.err.printf("[" + formatter.format(new Date(System.currentTimeMillis()))
-		+ "]-[" + x + "]-[%-"+LIMIT+"."+LIMIT+"s] %s%n", get(c.getSimpleName()), s);
+	private synchronized static String err(Class<?> c, String x, String s) {
+		/*System.err.printf("[" + formatter.format(new Date(System.currentTimeMillis()))
+		+ "]-[" + x + "]-[%-"+LIMIT+"."+LIMIT+"s] %s%n", get(c.getSimpleName()), s);*/
+		String log = "[" + formatter.format(new Date(System.currentTimeMillis()))
+		+ "]-[" + x +"]-[" + get(c.getSimpleName()).substring(0, LIMIT)  + "] " + s + "\n";
+		System.err.print(log);
+		return log;
 	}
 
 	/**
@@ -74,8 +106,8 @@ public class Log {
 	 * @param params first string will be key for the message stored in messages.properties & remaining will be parameters
 	 */
 	public static void i(Class<?> c, String ... params) {
-		out(c, ((params.length > 1) ? MessageFormat.format(messages.getProperty(params[0]), 
-				(Object[]) Arrays.copyOfRange(params, 1, params.length)) : messages.getProperty(params[params.length - 1])));
+		writeToFile(out(c, INF, ((params.length > 1) ? MessageFormat.format(messages.getProperty(params[0]), 
+				(Object[]) Arrays.copyOfRange(params, 1, params.length)) : messages.getProperty(params[params.length - 1]))));
 	}
 
 	/**
@@ -85,8 +117,8 @@ public class Log {
 	 * @param params first string will be key for the message stored in messages.properties & remaining will be parameters
 	 */
 	public static void w(Class<?> c, String ... params) {
-		err(c, "WRN", ((params.length > 1) ? MessageFormat.format(messages.getProperty(params[0]), 
-				(Object[]) Arrays.copyOfRange(params, 1, params.length)) : messages.getProperty(params[params.length - 1])));		
+		writeToFile(err(c, WRN, ((params.length > 1) ? MessageFormat.format(messages.getProperty(params[0]), 
+				(Object[]) Arrays.copyOfRange(params, 1, params.length)) : messages.getProperty(params[params.length - 1]))));		
 	}
 	
 	/**
@@ -96,8 +128,30 @@ public class Log {
 	 * @param params first string will be key for the message stored in messages.properties & remaining will be parameters
 	 */
 	public static void e(Class<?> c, String ... params) {
-		err(c, "ERR", ((params.length > 1) ? MessageFormat.format(messages.getProperty(params[0]), 
-				(Object[]) Arrays.copyOfRange(params, 1, params.length)) : messages.getProperty(params[params.length - 1])));		
+		writeToFile(err(c, ERR, ((params.length > 1) ? MessageFormat.format(messages.getProperty(params[0]), 
+				(Object[]) Arrays.copyOfRange(params, 1, params.length)) : messages.getProperty(params[params.length - 1]))));
+	}
+	
+	/**
+	 * ERROR LOG
+	 * Method will print log on system console using {@code System.err.println()}
+	 * @param c provide class, to know which log belongs to which class
+	 * @param e exception that arises 
+	 */
+	public static void e(Class<?> c, Exception exp) {
+		StringWriter sw = new StringWriter();
+	    exp.printStackTrace(new PrintWriter(sw));
+		writeToFile(err(c, ERR, sw.toString()));		
+	}
+	
+	/**
+	 * ERROR LOG
+	 * Method will print {@link KeyedMessage} log on system console using {@code System.out.println()}
+	 * @param c provide class, to know which log belongs to which class
+	 * @param msgs main content of the log
+	 */
+	public static void e(Class<?> c, KeyedMessage<String, String> msg) {
+		if (msg != null) writeToFile(err(c, ERR,  new String(msg.message())));		
 	}
 
 	/**
@@ -107,7 +161,7 @@ public class Log {
 	 * @param msg main content of the log
 	 */
 	public static void d(Class<?> c, String msg) {
-		out(c, msg);
+		out(c, DBG, msg);
 	}
 	
 	/**
@@ -117,10 +171,31 @@ public class Log {
 	 * @param msgs main content of the log
 	 */
 	public static void d(Class<?> c, KeyedMessage<String, String> msg) {
-		out(c, new String(msg.message()));		
+		if (msg != null) out(c, DBG, new String(msg.message()));		
 	}
 	
-	private static String get(String s) {		
+	/**
+	 * A method to write data on files. It will take decisions according to log levels attached with the logs e.g. ERR or INF.
+	 * Another thing is that file name will be a combination of dd-M-yyyy It mean log files will be generated daily.
+	 * @param log to write in files
+	 */
+	private static void writeToFile(String log) {
+		if (path != null) {
+			File file = new File(path);
+			file.mkdirs();
+			Calendar c = Calendar.getInstance();
+			try (FileWriter writer = new FileWriter(new File(file,
+					c.get(Calendar.DAY_OF_MONTH) + "-" + c.get(Calendar.MONTH) + "-" + c.get(Calendar.YEAR) + ".txt"
+					+ ((log.contains(ERR) || log.contains(WRN)) ? " ERR LOG " : " INF LOG ")
+					), true)) {
+				writer.write(log);
+			} catch (IOException e) {
+				e.printStackTrace();
+			} 
+		}
+	}
+	
+	private static String get(String s) {
 		if (s.length() < LIMIT)
 			for(int i = s.length(); i < LIMIT; i++)
 				s += ".";
